0b5a115c093a7500e02e264861b0c930ad9283a2
ppsrac <ppsracchriskim@gmail.com>
2023-01-15 01:09:23 +0900

    01150109

--- a.py
+++ a.py
@@ -49,5 +49,5 @@ def dfs(x):
             for num in promising:
                 sudoku[i][j] = num #유망한 숫자 중 하나를 넣어줌
                 dfs(x + 1) #다음 0으로 넘어감
-                sudoku[i][j] = 0 #초기화 (정답이 없을 경우를 대비)
+                sudoku[i][j] = 0
 dfs(0)
